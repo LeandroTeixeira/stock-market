a712a2d455d7b132dc234425381285918adc7a5c
Leandro Teixeira <leandroteixeira3@gmail.com>
2022-07-24 19:15:31 -0300

    refactor: removed unnecessary code

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,36 +23,7 @@ const loadTimeStocks = async (p) => {
   await mongoConnect();
 
   await loadTimeStocks(path.join(__dirname, '..', 'dbInitializer.csv'));
-  // const req = http.request({
-  //   host: 'localhost',
-  //   path: '/',
-  //   port: '5000',
-  //   method: 'POST',
-  //   body: {
-  //     var1: 'Teixeira, ',
-  //   },
-  // }, (res) => {
-  //   let str = '';
-  //   res.on('data', (chunk) => {
-  //     str += chunk;
-  //   });
 
-  //   res.on('end', () => {
-  //     console.log(str);
-  //   });
-  // });
-  // req.write('TEixeira, ');
-  // req.end();
-
-
-  // console.log(response.data.body.suggestions.suggestedBuy);
-  // .then((res) => {
-  //   console.log(`statusCode: ${res.status}`);
-  //   console.log(res.data);
-  // })
-  // .catch((error) => {
-  //   console.error(error);
-  // });
   server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
